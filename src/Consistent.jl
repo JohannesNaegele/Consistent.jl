e11ec3a5f026a0db75f1536430ed3d74e93eef89
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-09 02:09:20 +0200

    Specify includes and exports

--- src/Consistent.jl
+++ src/Consistent.jl
@@ -1,5 +1,10 @@
 module Consistent
 
-# Write your package code here.
+export @model, @endogenous, @exogenous, @parameters, @equations
+
+include("HelperFunctions.jl")
+include("Model.jl")
+include("Variables.jl")
+include("Macros.jl")
 
 end
