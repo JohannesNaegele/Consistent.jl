b77e265138e207a580991b9fda7ddf77179196fc
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-10 14:29:08 +0200

    Fix include

--- src/Consistent.jl
+++ src/Consistent.jl
@@ -2,7 +2,7 @@ module Consistent
 
 export @model, @endogenous, @exogenous, @parameters, @equations
 
-include("HelperFunctions.jl")
+include("Helpers.jl")
 include("Model.jl")
 include("Variables.jl")
 include("Macros.jl")
