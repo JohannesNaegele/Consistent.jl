83b7a1123d8d342bdd62dc41c277e608c1a60409
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-11 21:11:10 +0200

    Include predefined models

--- src/Consistent.jl
+++ src/Consistent.jl
@@ -8,4 +8,10 @@ include("Variables.jl")
 include("ConstructResiduals.jl")
 include("Macros.jl")
 
-end
+include("models/SIM.jl")
+include("models/SIM_stoch.jl")
+include("models/LP.jl")
+include("models/DIS.jl")
+include("models/PC.jl")
+
+end # module Consistent
