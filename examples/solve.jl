adba93690dc25110cfd9ef622483f420a87e2f92
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-13 02:11:48 +0200

    Fix issue with unintuitive lags/exos: Indexing in wrong direction

--- examples/solve.jl
+++ examples/solve.jl
@@ -18,20 +18,21 @@ model = Consistent.SIM() # load predefined SIM model
 params_dict = Consistent.SIM(true) # load default parameters
 exos = [20.0] # this is only G
 exos = exos[:, :] # bring in matrix format
-lags = fill(0.0, length(model.endogenous_variables)) # lagged values of endogenous variables are all 0.0
-lags = lags[:, :] # bring in matrix format
+lags = fill(0.0, length(model.endogenous_variables), 1) # lagged values of endogenous variables are all 0.0
 param_values = map(x -> params_dict[x], model.parameters) # get raw parameter values
 solution = solve(model, lags, exos, param_values) # solve first period
 
 # Solve model for 50 periods
 for i in 1:50
     solution = solve(model, lags, exos, param_values) # solve first period
-    lags = hcat(solution, lags)
+    lags = hcat(lags, solution)
 end
 
-df = DataFrame(reverse(lags, dims=2)', model.endogenous_variables)
+# Convert results to DataFrame
+df = DataFrame(lags', model.endogenous_variables)
 df[!, :period] = 1:nrow(df)
-df_long = @pipe df |>
+# Select variables, convert to long format, and plot variables
+@pipe df |>
     select(_, [:Y, :C, :YD, :period]) |>
     stack(_, Not(:period), variable_name=:variable) |>
     plot(
