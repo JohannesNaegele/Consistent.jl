12c0d1f86312e3e8b411c21e425327f659514e6d
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-27 03:24:03 +0200

    Add potential tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using Consistent
 using Test
 
 @testset "Consistent.jl" begin
-    # Write your tests here.
+    # @test test_model.exogenous_variables == [:G]
+    # Consistent.remove_expr([:x :(a in b) :y])
+
+    # a = quote
+    #     z = y*(y[-1] + 0.5*z)*θ + x[-1]
+    #     y = z[-2]*x*b
+    # end
+
+    # replace_vars(a.args[[2,4]], [:z, :y], Symbol[:x], [:θ])
 end
