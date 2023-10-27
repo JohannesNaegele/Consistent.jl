b9a9802d72ddb4854ea3360049380a29f644ba49
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-27 21:39:17 +0200

    Add solve file and file with new types for model components

--- src/Consistent.jl
+++ src/Consistent.jl
@@ -1,18 +1,21 @@
 module Consistent
 
-export @model, @endogenous, @exogenous, @parameters, @equations
+export @model, @endogenous, @exogenous, @parameters, @equations, @parameters
+export solve
 
 include("Helpers.jl")
+include("ModelComponents.jl")
 include("Model.jl")
 include("Variables.jl")
 include("ConstructResiduals.jl")
 include("Macros.jl")
 include("CombineModels.jl")
+include("Solve.jl")
 
-include("models/SIM.jl")
-include("models/SIM_stoch.jl")
-include("models/LP.jl")
-include("models/DIS.jl")
-include("models/PC.jl")
+# include("models/SIM.jl")
+# include("models/SIM_stoch.jl")
+# include("models/LP.jl")
+# include("models/DIS.jl")
+# include("models/PC.jl")
 
 end # module Consistent
