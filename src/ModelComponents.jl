762009f728b97ae77efaacf0c76d47aaeafdbecb
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-28 02:39:49 +0200

    New equations type

--- src/ModelComponents.jl
+++ src/ModelComponents.jl
@@ -10,8 +10,7 @@ Variables(x::OrderedDict) = Variables([k for (k, v) in x])
 Variables(x::Variables) = x
 Variables() = Variables(Symbol[])
 
-MacroTools.@forward Variables.variables Base.getindex, Base.setindex!
-Base.size(x::Variables) = Base.size(x.variables)
+MacroTools.@forward Variables.variables Base.getindex, Base.setindex!, Base.size
 
 macro variables(input...)
     if (input[1] isa Expr) && (input[1].head == :block)
@@ -24,10 +23,18 @@ macro variables(input...)
     end
 end
 
-struct Equations
-    expr::Expr
-end
+"""
+Macro to specify the parameters. Parameters typically can not change over time and can be calibrated to fit given data.
 
+Returns an OrderedDict.
+
+# Example:
+    @parameters begin
+        θ = 0.2
+        α_1 = 0.6
+        α_2 = 0.4
+    end
+"""
 macro parameters(block)
     exprs = block.args
 
@@ -37,5 +44,25 @@ macro parameters(block)
     # Extract variable names and their values as symbols with colons
     pairs = [Expr(:call, :(=>), :(Symbol($("$(a.args[1])"))), a.args[2]) for a in assignments]
 
-    return esc(Expr(:call, :OrderedDict, pairs...))
+    return esc(Expr(:call, :(Consistent.OrderedDict), pairs...))
+end
+
+struct Equations <: AbstractVector{Expr}
+    exprs::Vector{Expr}
+end
+
+MacroTools.@forward Equations.exprs Base.getindex, Base.setindex!, Base.size
+
+"""
+Macro to specify the model equations. Use `begin ... end`.
+
+# Example:
+    @equations begin
+        Y = G + C
+    end
+"""
+macro equations(input...)
+    ex = remove_blocks(MacroTools.striplines(input...)) # TODO: better debugging with LineNumberNodes
+    @assert (ex.head == :block) "Block input expected" # we need block input (begin ... end)
+    return Equations(deepcopy(ex.args))
 end
