d8591c59ae2748c52d333b0fa497e2cff9ab2f15
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-28 12:28:04 +0200

    Adapted combine functionality

--- src/CombineModels.jl
+++ src/CombineModels.jl
@@ -7,12 +7,10 @@ function +(model1::Model, model2::Model)
     exos2 = filter(
         x -> !((x in model1.endogenous_variables) || (x in exos1)), model2.exogenous_variables
     )
-    equations = vcat(model1.equations, model2.equations)
-    global Consistent.sfc_model = Model()
-    sfc_model.endogenous_variables = vcat(model1.endogenous_variables, model2.endogenous_variables)
-    sfc_model.exogenous_variables = vcat(exos1, exos2)
-    sfc_model.parameters = vcat(model1.parameters, model2.parameters)
-    sfc_model.equations = equations
-    eval(build_f!(equations))
-    deepcopy(sfc_model)
+    return model(
+        endos=Variables(vcat(model1.endogenous_variables, model2.endogenous_variables)),
+        exos=Variables(vcat(exos1, exos2)),
+        params=Variables(vcat(model1.parameters, model2.parameters)),
+        eqs=Equations(vcat(model1.equations, model2.equations))
+    )
 end
