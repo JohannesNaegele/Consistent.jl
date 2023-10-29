53eb6e1c261641c87a9411f5ef438bd8fc30e65e
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-29 02:34:19 +0200

    Adapt combine example

--- examples/combine.jl
+++ examples/combine.jl
@@ -1,23 +1,23 @@
 using Consistent
 
-PC_gdp = @model begin
-    @endogenous Y YD T V C
-    @exogenous r G B_h
-    @parameters α_1 α_2 θ
-    @equations begin
+PC_gdp = model(
+    endos = @variables(Y, YD, T, V, C),
+    exos = @variables(r, G, B_h),
+    params = @variables(α_1, α_2, θ),
+    eqs = @equations begin
         Y = C + G
         YD = Y - T + r[-1] * B_h[-1]
         T = θ * (Y + r[-1] * B_h[-1])
         V = V[-1] + (YD - C)
         C = α_1 * YD + α_2 * V[-1]
     end
-end
+)
 
-PC_hh = @model begin
-    @endogenous H_h B_h B_s H_s B_cb r
-    @exogenous r_exo G V YD T
-    @parameters λ_0 λ_1 λ_2
-    @equations begin
+PC_hh = model(
+    endos = @variables(H_h, B_h, B_s, H_s, B_cb, r),
+    exos = @variables(r_exo, G, V, YD, T),
+    params = @variables(λ_0, λ_1, λ_2),
+    eqs = @equations begin
         H_h = V - B_h
         B_h = (λ_0 + λ_1 * r - λ_2 * (YD / V)) * V
         B_s = (G + r[-1] * B_s[-1]) - (T + r[-1] * B_cb[-1]) + B_s[-1]
@@ -25,6 +25,7 @@ PC_hh = @model begin
         B_cb = B_s - B_h
         r = r_exo
     end
-end
+)
 
+# Note: Since the variables and equations are ordered this operation is not commutative!
 PC_complete = PC_gdp + PC_hh
