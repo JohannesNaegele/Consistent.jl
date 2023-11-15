fafc1fa7401053d0aeac5677e92e8a2d1e8f1ef4
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-11-15 23:32:31 +0100

    Extend ideas to calibrate

--- examples/calibrate.jl
+++ examples/calibrate.jl
@@ -1,15 +1,41 @@
 using Consistent
 using DataFrames
-using ForwardDiff
+using Zygote
+using Optimization
+using OptimizationOptimJL
 
-function calibrate!(data, model, parameters=Dict())
-    x -> model.f!(y, x, ...) + x
-    function loss(data,)
-
+function calibrate!(data, model, parameters=Dict(), initial_parameters=Dict())
+    exos = data...
+    lags = ...
+    F = similar(...)
+    function g!(F, x)
+        return model.f!(F, x, lags, exos, params)
     end
-    ForwardDiff.derivative(() -> loss, input)
+    function loss(params)
+        sol = solve(model, lags, exos, params; initial=fill(1.0, length(model.endogenous_variables)), method=:newton)
+    end
+    deriv = x -> ForwardDiff.derivative(loss, x)
+    initial = ...
+    prob = OptimizationProblem(loss, deriv, initial, p)
+    solve(prob, BFGS())
 end
 
-model = SIM()
-df = DataFrame(reverse(lags[:, end-20:end-1], dims=2)', model.endogenous_variables)
-df
+# Given data for exogenous variables
+df = DataFrame(:G => 20 .+ 10 * rand(60))
+
+# Define model
+sim = Consistent.SIM()[:model]
+exos = permutedims(Matrix(df[!, sim.exogenous_variables]))
+lags = Consistent.SIM()[:lags]
+params_dict = Consistent.SIM()[:params]
+param_values = map(x -> params_dict[x], sim.parameters)
+
+# Solve model for 59 periods
+for i in 1:59
+    # assume we have some randomness in our parameters
+    solution = Consistent.solve(sim, lags, exos[:, begin:i], param_values + 0.1 * rand(3))
+    lags = hcat(lags, solution)
+end
+
+# Convert results to DataFrame
+df = hcat(df, DataFrame(lags', sim.endogenous_variables))
