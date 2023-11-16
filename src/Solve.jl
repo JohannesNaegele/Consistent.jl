a8b3a1a9a4075f1f59d1a37bcb66e800a5537a83
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-11-16 03:39:22 +0100

    New nonlinear solve variant

--- src/Solve.jl
+++ src/Solve.jl
@@ -1,9 +1,38 @@
 using NLsolve
+using LinearAlgebra
+import Optimization
+import Optim
+using NonlinearSolve
+using SimpleNonlinearSolve
 
-function solve(model, lags, exos, params, initial=fill(1.0, length(model.endogenous_variables)))
+function solve(model, lags, exos, params; initial=fill(1.0, length(model.endogenous_variables)), method=:newton)
     nlsolve(
-        (x, y) -> model.f!(x, y, lags, exos, params),
+        (F, x) -> model.f!(F, x, lags, exos, params),
         initial,
         autodiff = :forward,
+        method = method,
+        # iterations = 500,
+        ftol = 1e-40,
+        xtol = 1e-40
     ).zero
 end
+
+function solve_nonlinear(model, lags, exos, params; initial=fill(1.0, length(model.endogenous_variables)), method=:newton)
+    prob = NonlinearSolve.NonlinearProblem(
+        (F, x, p) -> model.f!(F, x, lags, exos, params),
+        initial,#
+        abstol = 1e-40, reltol = 1e-40
+    )
+    sol = NonlinearSolve.solve(prob, NonlinearSolve.TrustRegion())
+    return sol
+end
+
+# function solve_optim(model, lags, exos, params; initial=fill(1.0, length(model.endogenous_variables)), method=:newton)
+#     f = (x, y) -> model.f!(x, y, lags, exos, params)
+#     prob = Optimization.OptimizationProblem(
+#         x -> max(norm(f(x, y)) + abs(f(x, y)[1] - f(x, y)[1])),
+#         initial,
+#         Optimization.AutoForwardDiff()
+#     )
+#     sol = Optimization.solve(prob, Optim.BFGS())
+# end
